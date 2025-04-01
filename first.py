430686da6d338e8267e85a8b16754632b677f597
zakiya khan <khatoonzakiya03@gmail.com>
2025-04-01 15:08:59 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,10 @@
+print("hello guys")
+print("First-program python print Function ")
+print("it is declare like this:")
+print("print('what to print')")
+
+print("the assignment question")
+print("String Mnupulation Exercise \nString cancatination is done with "+"sign")
+print('e.g print("hello"+"zakiya")')
+print("New line can be created by using backslace n")
+
